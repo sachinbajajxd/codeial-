0a1b73153c827d7a8a1ee56e4357e7da9e140bc1
Sachin <sachinbajaj0001@gmail.com>
2022-09-17 23:47:47 +0530

    add manual auth

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,6 @@ router.get('/sign-in',usersController.sign_in);
 
 router.post('/create',usersController.create);
 
+router.post('/create-session',usersController.createSession);
+
 module.exports = router;
