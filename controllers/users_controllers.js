447980d311f000d305120cdff7bbdc50c2493c74
Sachin <sachinbajaj0001@gmail.com>
2022-10-09 18:47:31 +0530

    Add flash message

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -87,6 +87,7 @@ module.exports.create = function(req,res){
 
 //sign in and create a session for the user
 module.exports.createSession = function(req,res){
+    req.flash('success', 'Logged in successfully');
     return res.redirect('/');
 };
 
@@ -96,6 +97,7 @@ module.exports.deleteSession = function(req,res){
         if(err){
             return console.log(err);
         }
+        req.flash('success', 'You have logged out!');
         return res.redirect('/');
     });
 };
--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo');
 const sassMiddleware = require('node-sass-middleware');
+const flash = require('connect-flash');
+const customMware = require('./config/middleware');
 
 app.use(sassMiddleware({
     src: './assets/scss',
@@ -66,6 +68,8 @@ app.use(passport.session());
 
 app.use(passport.setAuthenticatedUser);
 
+app.use(flash());
+app.use(customMware.setFlash);
 
 //importing routes from ./routes/index.js
 //Now for every request it will go in routes folder
