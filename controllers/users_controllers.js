1ce390962c0b289016b3d4955a958c12367f1a45
Sachin <sachinbajaj0001@gmail.com>
2022-10-16 23:59:19 +0530

    Add multer for file upload 1

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -14,13 +14,44 @@ module.exports.profile = function(req,res){
 
 };
 
-module.exports.update = function(req,res){
+module.exports.update = async function(req,res){
     
+    // if(req.user.id == req.params.id){
+    //     User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+    //         req.flash('success', 'Updated!');
+    //         return res.redirect('back');
+    //     });
+    // }else{
+    //     req.flash('error', 'Unauthorized!');
+    //     return res.status(401).send('Unautorized');
+    // }
+
     if(req.user.id == req.params.id){
-        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
-            req.flash('success', 'Updated!');
+
+        try{
+
+            let user = await User.findByIdAndUpdate(req.params.id);
+            User.uploadedAvatar(req, res, function(err){
+                if(err){
+                    console.log('*******Multer Error****', err);
+                }
+                // console.log(req.file);
+                user.name=req.body.name;
+                user.email=req.body.email;
+
+                if(req.file){
+                    //saving the path of the uplaoded file into the avatar field in the user
+                    user.avatar = User.avatarPath + '/' + req.file.filename;
+                }
+                user.save();
+                return res.redirect('back');
+            });
+
+        }catch(err){
+            req.flash('error', err);
             return res.redirect('back');
-        });
+        }
+
     }else{
         req.flash('error', 'Unauthorized!');
         return res.status(401).send('Unautorized');
