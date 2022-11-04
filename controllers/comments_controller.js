f4124cf4ae9ce33c7c44b70050f70ccd590b51d0
Sachin <sachinbajaj0001@gmail.com>
2022-11-04 22:18:06 +0530

    Add like functionality

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -4,6 +4,7 @@ const Post = require('../models/post');
 const commentsMailer=require('../mailers/comments_mailer');
 const commentEmailWorker=require('../workers/comment_email_worker');
 const queue=require('../config/kue');
+const Like=require('../models/like');
 
 
 module.exports.create = async function(req,res){
@@ -73,6 +74,9 @@ module.exports.destroy = async function(req,res){
             comment.remove();
             
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
+
+            //destroy the associated likes for the comment
+            await Like.deleteMany({likeable: comment._id, onModel: 'Comment'});
 
             // send the comment id which was deleted back to the views
             if (req.xhr){
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,6 +1,7 @@
 
 const Post =require('../models/post');
 const Comment = require('../models/comment');
+const Like=require('../models/like');
 
 module.exports.create = async function(req,res){
 
@@ -29,7 +30,7 @@ module.exports.create = async function(req,res){
 
     }catch(err){
 
-        // console.log('Error', err);
+        console.log('Error', err);
         req.flash('error', err);
         return res.redirect('back');
 
@@ -43,8 +44,12 @@ module.exports.destroy = async function(req,res){
         let post=await Post.findById(req.params.id);
 
         if(post.user == req.user.id){
-            post.remove();
 
+            await Like.deleteMany({likeable: post, onModel: 'Post'});
+            await Like.deleteMany({_id: {$in: post.comments}});
+
+            post.remove();
+            
             await Comment.deleteMany({post: req.params.id});
 
             if(req.xhr){
